6853e9ba6c68c9aa23cecc02af7369fa21e1d737
Diego Acosta <d.acostao@uniandes.edu.co>
2025-03-15 08:41:38 -0500

    Fix initial activity layout

---
+++ Alarmify/app/src/main/java/com/misw43022025/alarmify/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.misw43022025.alarmify;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // Establece login.xml como el layout principal
+    }
+}
